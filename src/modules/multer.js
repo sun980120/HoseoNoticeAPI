8a403440ec792e8b9a8c8fe15c950da53fba4cc4
WooSungHong <bin980120@gmail.com>
2022-08-08 14:23:39 +0900

    notice : edit routes, controller, dao

--- src/modules/multer.js
+++ src/modules/multer.js
@@ -1,4 +1,11 @@
 import multer from 'multer' // 1
+import fs from 'fs';
+fs.readdir("uploads", (error) => {
+    if (error) {
+        console.error("uploads폴더가 없어 uploads폴더를 생성합니다.");
+        fs.mkdirSync('src/public/uploads');
+    }
+});
 const storageNotice = multer.diskStorage({ // 2
     destination(req, file, cb) {
         cb(null, 'src/public/uploads/');
